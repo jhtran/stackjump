17c276a87a97fb3f9f4ab6cf5d63b1cf7774e6f8
John Tran <jhtran@att.com>
2012-04-18 17:22:57 -0700

    adding test mode, skips the downloads of linux, initrid & isolinux.bin instead creates fake stubs

--- tests/stackjump-1-test.sh
+++ tests/stackjump-1-test.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env roundup
+#!/usr/local/bin/roundup
 
 PATH="$PATH:.."
-alias sj="stackjump"
+alias sj="stackjump -t "
+
+randomf() {
+  echo "/tmp/`cat /dev/urandom| tr -dc 'a-zA-Z0-9' | fold -w 10| head -n 1`"
+}
 
 it_reqs_args() {
   ! sj
@@ -10,4 +14,15 @@ it_reqs_args() {
 it_displays_usage() {
   OUT=`sj|head -2|tail -1`
   expr "$OUT" : '  -p preseed'
+}
+
+it_needs_real_preseed() {
+  RANDOM=`randomf`
+  ! OUT=`sj -p $RANDOM`
+  test "$OUT" = "$RANDOM not a valid preseed file"
+}
+
+it_not_allow_dir_and_git() {
+  ! OUT=`sj -d /tmp -g git@github.com/user/proj`
+  test "$OUT" = 'Use only one of the -d or -g flags.'
 }
