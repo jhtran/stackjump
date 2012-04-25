a8abca8e13dffa7de2679ff6ff11e83d2713ac5b
John Tran <jhtran@att.com>
2012-04-25 12:47:31 -0700

    updated test

--- tests/stackjump-1-test.sh
+++ tests/stackjump-1-test.sh
@@ -151,7 +151,7 @@ it_uses_preseed_arg_over_github() {
 
 it_uses_valid_github_preseed() {
   TMPDIR=`sj -g $GITREPO -k|grep Temp|awk '{print $3}'`
-  expr "`head -2 $TMPDIR/initrd/preseed.cfg|tail -1`" : '.*debian-installer\/locale'
+  expr "`head -3 $TMPDIR/initrd/preseed.cfg|tail -1`" : '.*debian-installer\/locale'
   teardown $TMPDIR
 }
 
